0bcca7c5ddcb29f1fbf895f8af1619c2685eb232
tales138 <tales.oliveira.rodrigues61@aluno.ifce.edu.br>
2023-07-16 21:31:07 -0300

    adicionando estilo ao primeiro widget

--- widget_one.py
+++ widget_one.py
@@ -7,7 +7,12 @@ class Application:
         self.widget1 = ttk.Frame(master)
         self.widget1.pack()
         self.msg = ttk.Label(self.widget1,text="Primeiro Wiget")
+        self.msg["font"] = ("Verdana",10,"italic","bold")
         self.msg.pack()
+        self.sair = ttk.Button(self.widget1, text="Sair", command=self.widget1.quit)
+        style = ttk.Style()
+        style.configure("TButton", font=("Calibri", 10))
+        self.sair.pack()
 
 root = tk.Tk()
 
