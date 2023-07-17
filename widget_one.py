0799fbe387a192bfe64d850b661d26cecd827caf
tales138 <tales.oliveira.rodrigues61@aluno.ifce.edu.br>
2023-07-16 21:19:35 -0300

    primeiro widget

--- widget_one.py
+++ widget_one.py
@@ -3,8 +3,11 @@ from tkinter import ttk
 
 
 class Application:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self, master=None):
+        self.widget1 = ttk.Frame(master)
+        self.widget1.pack()
+        self.msg = ttk.Label(self.widget1,text="Primeiro Wiget")
+        self.msg.pack()
 
 root = tk.Tk()
 
