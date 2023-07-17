a69154d2281e7c0d816612955c41f91da21cac9e
tales138 <tales.oliveira.rodrigues61@aluno.ifce.edu.br>
2023-07-16 21:09:20 -0300

    gerando container

---
+++ widget_one.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class Application:
+    def __init__(self, root):
+        self.root = root
+
+root = tk.Tk()
+
+Application(root)
+root.mainloop()
