cbb556a4d48df2ae8e038dbafd9e5c98aae1a608
tales138 <tales.oliveira.rodrigues61@aluno.ifce.edu.br>
2023-07-17 21:12:42 -0300

    event handler no widget

--- widget_one.py
+++ widget_one.py
@@ -6,13 +6,20 @@ class Application:
     def __init__(self, master=None):
         self.widget1 = ttk.Frame(master)
         self.widget1.pack()
-        self.msg = ttk.Label(self.widget1,text="Primeiro Wiget")
+        self.msg = ttk.Label(self.widget1,text="Primeiro widget")
         self.msg["font"] = ("Verdana",10,"italic","bold")
         self.msg.pack()
-        self.sair = ttk.Button(self.widget1, text="Sair", command=self.widget1.quit)
+        self.sair = ttk.Button(self.widget1,text="Clique aqui")
         style = ttk.Style()
-        style.configure("TButton", font=("Calibri", 10))
-        self.sair.pack()
+        style.configure("TButton",font=("Calibri", 10),width=20)
+        self.sair.bind("<Button-1>",self.mudarTexto)
+        self.sair.pack(side="right")
+    def mudarTexto(self,event):
+        if self.msg["text"] == "Primeiro widget":
+            self.msg["text"] = "O botão recebeu um clique"
+           
+        else:
+            self.msg["text"] = "Primeiro Widget"
 
 root = tk.Tk()
 
